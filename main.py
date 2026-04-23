aa953003181869e39580a3c86ec5c0974db07f5d
Reeni Rivera <thereeni.rivera@outlook.com>
2026-04-22 19:44:53 -0500

    added json file

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify , request
+import json
+
+with open('data.json') as f:
+    data = json.load(f)
 
 app = Flask(__name__)
 
 @app.route("/")
 def Welcome():
-    return "Welcome to the hardcoded API"
+    return "<b>Welcome to the Teach API<b>"
+
+@app.route("/students")
+def Students():
+    return jsonify(data["students"])
